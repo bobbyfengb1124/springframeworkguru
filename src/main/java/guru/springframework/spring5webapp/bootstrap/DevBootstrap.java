d29b1ab8583528c2a6abbed6b48e6a4311de1ad6
bobbyfeng <bobbyfeng@Bobby01-NB.cse.com.sg>
2017-08-02 17:29:50 +0800

    added publisher

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -6,36 +6,46 @@ import org.springframework.stereotype.Component;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 
 @Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
 	private AuthorRepository authorRepository;
 	private BookRepository bookRepository;
+	private PublisherRepository publisherRepository;
 
-	public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+	public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository,
+			PublisherRepository publisherRepository) {
 		this.authorRepository = authorRepository;
 		this.bookRepository = bookRepository;
+		this.publisherRepository = publisherRepository;
 	}
 
 	private void initData() {
 		// Eric
 		Author eric = new Author("Eric", "Evans");
-		Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+		Publisher harper = new Publisher("Harper Collins", "London");
+		Book ddd = new Book("Domain Driven Design", "1234", harper);
 		eric.getBooks().add(ddd);
 		ddd.getAuthors().add(eric);
 
 		authorRepository.save(eric);
+		publisherRepository.save(harper);
 		bookRepository.save(ddd);
+
 		// Rod
 
 		Author rod = new Author("Rod", "Johnson");
-		Book noEJB = new Book("J2EE Development without EJB", "23444", "Worx");
+		Publisher worx = new Publisher("Worx", "Singapore");
+		Book noEJB = new Book("J2EE Development without EJB", "12345", worx);
 		rod.getBooks().add(noEJB);
 
 		authorRepository.save(rod);
+		publisherRepository.save(worx);
 		bookRepository.save(noEJB);
 	}
 
--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -10,6 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Book {
@@ -19,7 +20,9 @@ public class Book {
 	private Long id;
 	private String title;
 	private String isbn;
-	private String publisher;
+
+	@OneToOne
+	private Publisher publisher;
 
 	@ManyToMany
 	@JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
@@ -28,14 +31,7 @@ public class Book {
 	public Book() {
 	}
 
-	public Book(String title, String isbn, String publisher, Set<Author> authors) {
-		this.title = title;
-		this.isbn = isbn;
-		this.publisher = publisher;
-		this.authors = authors;
-	}
-
-	public Book(String title, String isbn, String publisher) {
+	public Book(String title, String isbn, Publisher publisher) {
 		this.title = title;
 		this.isbn = isbn;
 		this.publisher = publisher;
@@ -65,11 +61,11 @@ public class Book {
 		this.isbn = isbn;
 	}
 
-	public String getPublisher() {
+	public Publisher getPublisher() {
 		return publisher;
 	}
 
-	public void setPublisher(String publisher) {
+	public void setPublisher(Publisher publisher) {
 		this.publisher = publisher;
 	}
 
